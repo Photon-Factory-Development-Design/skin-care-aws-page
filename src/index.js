4d5be67106107bf2e5c91c8447ce82b4306ca986
TalentWebDev <49565243+talentwebdev@users.noreply.github.com>
2021-11-15 13:36:17 -0400

    Build updates of responsive

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { QuizContainer } from 'containers';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <QuizContainer />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
